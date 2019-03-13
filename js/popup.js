49208114950dbc6114a9e7551830e0cce66fa415
Karbonara <37299335+karbonara@users.noreply.github.com>
2019-03-13 15:05:33 +0300

    Внес правки

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,11 @@
 var link = document.querySelector(".button-footer");
 var popup = document.querySelector(".modal-form");
 var close = popup.querySelector(".modal-close");
-
-link.addEventListener("click", function (evt) {
+link.addEventListener("click", function(evt) {
    evt.preventDefault();
    popup.classList.add("modal-show");
 });
-
-  close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-show");
-  });
-
+close.addEventListener("click", function(evt) {
+   evt.preventDefault();
+   popup.classList.remove("modal-show");
+});
